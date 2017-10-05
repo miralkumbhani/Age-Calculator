cc6cbe3d88d5a359f588629e8f0de63aa09f4383
Miral Kumbhani <miral.kumbhani@zymr.com>
2017-10-05 19:02:33 +0530

    Refactored the functions and JS in PROMISE and ES6 format

--- background.js
+++ background.js
@@ -1,20 +1,8 @@
 'use strict';
 
-$(document).ready(function() {
+$(document).ready(() => {
     $('.tabcontent').eq(0).show();
-
-    let age_day = _setDay();
-    $('#date_label').html(age_day.join(''));
-
-    let age_month = _setMonth();
-    $('#month_label').html(age_month.join(''));
-
-    let age_year = _setYear();
-    $('#year_label').html(age_year.join(''));
-
-    const currDate = new Date();
-    const dayInMilli = 1000 * 60 * 60 * 24;
-
+    // works on top tabs to switch between tabs
     $('.tab').on('click', '.tablinks', function() {
         let display = $(this).data('display');
         let index = $(this).index();
@@ -24,43 +12,19 @@ $(document).ready(function() {
         $('#tab_data').children('#' + display).show();
     });
 
-    $(document).on('click', 'button.funButton', function() {
-        //something happens and add_on gets a code based on either form name or button click
-        if (add_on === true) {
-            let start_date = document.firstForm.start_date.value;
-            let start_month = document.firstForm.start_month.value;
-            let start_year = document.firstForm.start_year.value;
-
-            let userSDateString = `${start_month}-${start_date}-${start_year}`;
-            let userSDate = new Date(userSDateString);
-
-            let userEDate = currDate;
-        } else if (add_on === false) {
-            let start_date = document.secondForm.start_date.value;
-            let start_date = document.secondForm.start_month.value;
-            let start_year = document.secondForm.start_year.value;
-
-            let end_date = document.secondForm.end_date.value;
-            let end_month = document.secondForm.end_month.value;
-            let end_year = document.secondForm.end_year.value;
-
-            let userSDateString = `${start_month}-${start_date}-${start_year}`;
-            let userSDate = new Date(userSDateString);
-
-            let userEDateString = `${end_month}-${end_date}-${end_year}`;
-            let userEDate = new Date(userEDateString);
-        }
-        getDiffTime(userSDate, userEDate, add_on); //add_on=true for Age Diff, add_on=false for Time Diff
-    });
-
-
-    /* ========================= FOR TAB1: Birth Date Age Calculator =========================== */
-    $(document).on('click', 'button.funButton', function() {
-        //getAge(fd, sd , true);
-    });
-
-    /* =========================== FOR TAB2: Different Dates Age Calculator ======================= */
-    $(document).on('click', 'button#getDiff', function() {
-        //getDateDiff(fd, sd, false);
+    // Promise.all will resolve when all promise resolved
+    Promise.all([_setDay, _setMonth, _setYear]).then( (result) => {
+        // console.log("result", result);
+        //creating date option list
+        let dayList = result[0].join('');
+        $('.start-date').html(dayList);
+        //creating month option list
+        let monthList = result[1].join('');
+        $('.start-month').html(monthList);
+        //creating year option list
+        let yearList = result[2].join('');
+        $('.start-year').html(yearList);
+    }).catch((err) => {
+        console.log('one of promise rejected', err);
     });
 });
--- mainscript.js
+++ mainscript.js
@@ -1,69 +1,78 @@
+'use strict';
+
 const currDate = new Date();
 const dayInMilli = 1000 * 60 * 60 * 24;
 
 // @return array = [1,...31]
-function _setDay() {
+let _setDay = new Promise((resolve, reject) => {
     let dateArray = [];
     for (let j = 1; j < 32; j++) {
         dateArray.push(j);
     }
-    let dateDropdown = [];
+    let dateList = [];
     $.each(dateArray, function(i, idx) {
-        dateDropdown.push(`<option value="${i+1}">${idx}</option>`);
+        dateList.push(`<option value="${i+1}">${idx}</option>`);
     });
-    return dateDropdown;
-}
+    resolve(dateList);
+    // let rr = {rj:'this is gone'};
+    // reject(rr);
+});
 
 //@return array = [January, ...December]
-function _setMonth() {
+let _setMonth = new Promise((resolve, reject) => {
     let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let monthDropdown = [];
+    let monthList = [];
     $.each(monthArray, function(i, idx) {
-        monthDropdown.push(`<option value="${i+1}">${idx}</option>`);
+        monthList.push(`<option value="${i+1}">${idx}</option>`);
     });
-    return monthDropdown;
-}
+    resolve(monthList);
+});
 
 //@return array = [currentYear,....0]
-function _setYear() {
+let _setYear = new Promise((resolve, reject) => {
     let yearArray = [];
     let endYear = new Date().getFullYear();
-    for (let j = endYear; j > 1599; j--) {
+    for (let j = endYear; j > 1857; j--) {
         yearArray.push(j);
     }
-    let yearDropdown = [];
+    let yearList = [];
     $.each(yearArray, function(i, idx) {
-        yearDropdown.push(`<option value="${idx}">${idx}</option>`);
+        yearList.push(`<option value="${idx}">${idx}</option>`);
     });
-    return yearDropdown;
-}
+    resolve(yearList);
+});
 
 //function to display the output properly
 const maybePluralize = (count, noun, suffix = 's') => `${count} ${noun}${count !== 1 ? suffix : ''}`;
 
 //function to calculate the zodiac sign based on birth date
-const zodiac = function(day, month) {
+const zodiac = (day, month) => {
     let zodiacName = ['Capricorn', 'Aquarius', 'Pisces', 'Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn'];
     let last_day = [19, 18, 20, 20, 21, 21, 22, 22, 21, 22, 21, 20, 19];
     return (day > last_day[month]) ? zodiacName[month * 1 + 1] : zodiacName[month];
 };
 
-//function for the countdown time for birthday
-const getCountdownTime = function(inputDate) {
+//function for the countdown time for birthday (CHECK)
+const getCountdownTime = (inputDate) => {
     //function to get the countdown to next birthday
-    let userDate = inputDate;
-    let dlDay = userDate.getDate();
-    let dlMon = userDate.getMonth();
-    let dlYr = currDate.getFullYear() + 1;
+     let userDate = inputDate;
+    // let dlDay = userDate.getDate();
+    // let dlMon = userDate.getMonth();
+    // let dlYr = currDate.getFullYear() + 1;
 
-    let deadline = new Date();
-    deadline.setDate(dlDay);
-    deadline.setMonth(dlMon);
-    deadline.setYear(dlYr);
+    // let deadline = new Date();
+    // deadline.setDate(dlDay);
+    // deadline.setMonth(dlMon);
+    // deadline.setYear(dlYr);
+
+    let [x, y, z] = [userDate.getDate(), userDate.getMonth(), currDate.getFullYear() + 1];
+    console.log("x, y, z:", x, y, z);
+    let countdownTime = new Date(x, y, z).getTime();
+    // let countdownTime = new Date(deadline).getTime();
 
     let interval = setInterval(function() {
         let currentTime = new Date().getTime();
-        let countdownTime = new Date(deadline).getTime();
+
         let diffInTime = countdownTime - currentTime;
 
         let days = Math.floor(diffInTime / dayInMilli);
@@ -82,17 +91,17 @@ const getCountdownTime = function(inputDate) {
 };
 
 //function to get zodiac data from json
-const getZodiacData = function() {
-    return $.getJSON("./zodiac.json").done(function(data) {
+const getZodiacData = () => {
+    return $.getJSON("./zodiac.json").done((data) => {
         return data;
-    }).fail(function(err) {
+    }).fail( (err) => {
         console.log('error while parsing json', err);
         return false;
     });
 };
 
 //function for getting zodiac details
-function getZodiacDetails(z, zname) {
+const getZodiacDetails = (z, zname) => {
     let zArray = z;
     let zodiacName = zname;
     let selected_array = zArray.find(function(z) {
@@ -104,7 +113,7 @@ function getZodiacDetails(z, zname) {
 }
 
 //function to get complete age from today
-function getTrueAge(inputDate) {
+const getTrueAge = (inputDate) => {
     let userDate = inputDate;
     let ageYears, ageMonths, ageDays;
     //days calculation from today
@@ -135,39 +144,73 @@ function getTrueAge(inputDate) {
     $('#user_todayAge').html(totalAge);
 }
 
-/* ====================================================================================================== */
+//function to get data for calculating the time diff
+const getDataDiff = (formName, addon) => {
+    let form_name = formName;
+    let add_on = addon;
+
+    if (add_on === true && form_name === 'firstForm') {
+        let startDate = document.firstForm.start_date.value;
+        let startMonth = document.firstForm.start_month.value;
+        let startYear = document.firstForm.start_year.value;
+
+        let userSDateString = `${startMonth}-${startDate}-${startYear}`;
+        let userSDate = new Date(userSDateString);
+
+        let userEDate = currDate;
+
+        getDiffTime(userSDate, userEDate, add_on);
+    } else if (add_on === false && form_name === 'secondForm') {
+        let startDate = document.secondForm.start_date.value;
+        let startMonth = document.secondForm.start_month.value;
+        let startYear = document.secondForm.start_year.value;
+
+        let endDate = document.secondForm.end_date.value;
+        let endMonth = document.secondForm.end_month.value;
+        let endYear = document.secondForm.end_year.value;
+
+        let userSDateString = `${startMonth}-${startDate}-${startYear}`;
+        let userSDate = new Date(userSDateString);
+
+        let userEDateString = `${endMonth}-${endDate}-${endYear}`;
+        let userEDate = new Date(userEDateString);
+
+        getDiffTime(userSDate, userEDate, add_on);
+    }
+}
+
 //one common function for getting Age Diff and Time Diff
-function getDiffTime(initial_date, final_date, op) {
-    let userSDate = initial_date;
-    let userEDate = final_date;
+const getDiffTime = (initial_date, final_date, op) => {
+    let userStartDate = initial_date;
+    let userEndDate = final_date;
     let add_on = op;
     let end_year = final_date.getFullYear();
     let start_year = initial_date.getFullYear();
 
-    if (isFinite(userSDate) && isFinite(userEDate)) {
-        if (end_year > start_year) {
+    if (isFinite(userStartDate) && isFinite(userEndDate)) {
+        if (end_year >= start_year) {
             //for calculating diff in years
-            let diffYears = userEDate.getFullYear() - userSDate.getFullYear();
+            let diffYears = userEndDate.getFullYear() - userStartDate.getFullYear();
             //for calculating diff in months
-            let diffMonths = (diffYears) * 12 + (userEDate.getMonth() - userSDate.getMonth());
+            let diffMonths = (diffYears) * 12 + (userEndDate.getMonth() - userStartDate.getMonth());
             //for calculating diff in weeks
-            let selectTime = userEDate.getTime();
-            let userTime = userSDate.getTime();
+            let selectTime = userEndDate.getTime();
+            let userTime = userStartDate.getTime();
             let diffTime = Math.abs(selectTime - userTime);
             let diffWeeks = Math.floor(diffTime / (dayInMilli * 7));
             //for calculating diff in days
-            let diffDays = Math.floor((userEDate - userSDate) / dayInMilli);
+            let diffDays = Math.floor((userEndDate - userStartDate) / dayInMilli);
 
             if (add_on === true) {
                 $('#today_date').html(currDate);
-                $('#user_date').html(userSDate);
+                $('#user_date').html(userStartDate);
                 $('#user_ageYears').html(diffYears);
                 $('#user_ageMonths').html(diffMonths);
                 $('#user_ageWeeks').html(diffWeeks);
                 $('#user_ageDays').html(diffDays);
-                getTrueAge(userSDate);
-                getCountdownTime(userSDate);
-                let z_sign = zodiac(userSDate.getDate(), userSDate.getMonth());
+                getTrueAge(userStartDate);
+                getCountdownTime(userStartDate);
+                let z_sign = zodiac(userStartDate.getDate(), userStartDate.getMonth());
                 let zodiac_info = [];
                 getZodiacData().then((d) => {
                     zodiac_info = d;
@@ -177,19 +220,19 @@ function getDiffTime(initial_date, final_date, op) {
                     $('#zodiac_attributes').html(displayZodiac.attribute);
                 });
             } else if (add_on === false) {
-                $('#start_date').html(userSDate);
-                $('#end_date').html(userEDate);
+                $('#start_date').html(userStartDate);
+                $('#end_date').html(userEndDate);
                 $('#diff_years').html(diffYears);
                 $('#diff_months').html(diffMonths);
                 $('#diff_weeks').html(diffWeeks);
                 $('#diff_days').html(diffDays);
             }
+        } else {
+            alert('The Selected Year should be greater than the Birth year');
+            return false;
         }
     } else {
-        alert('The Selected Year should be greater than the Birth year');
+        alert('Invalid Date, try again.');
         return false;
     }
-} else {
-    alert('Invalid Date, try again.');
-    return false;
 }
